9da368ccc5d9db29fc67a9b348b0fef8b087325d
Jesus Villegas <jesus_villegas@intuit.com>
2019-08-15 09:40:27 -0700

    Adding the function names we will use

---
+++ src/ProgrammingLevelOne.java
@@ -0,0 +1,19 @@
+public class ProgrammingLevelOne {
+
+    public static void main(String[] args) {
+        System.out.println("mdsrae\n"
+            + "alieyb\n"
+            + "okieosc\n"
+            + "221112\n"
+            + "drizwa\n"
+            + "girblae\n"
+            + "ckyuch\n"
+            + "ornoclt\n"
+            + "weysle\n"
+            + "unjail");
+    }
+
+    public static String unscramble(String combined) {
+        return null;
+    }
+}
